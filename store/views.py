c2854ac0b2f7dd15ce63cb02f1010dd66c94bb17
Leetzner <ognill@hotmail.com>
2023-02-28 14:18:45 +0000

    added cart view and page

--- store/views.py
+++ store/views.py
@@ -1,7 +1,31 @@
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+
+from .cart import Cart
 from .models import Product, Category
+
+
+def add_to_cart(request, product_id):
+    cart = Cart(request)
+    cart.add(product_id)
+
+    return redirect('frontpage')
+
+
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+    cart.remove(product_id)
+
+    return redirect('cart_view')
+
+
+def cart_view(request):
+    cart = Cart(request)
+
+    return render(request, 'store/cart_view.html', {
+        'cart': cart
+    })
 
 
 def search(request):
